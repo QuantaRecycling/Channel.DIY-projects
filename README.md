ac93b6701aee695ccc7540116ab96566e41a205e
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-27 00:12:05 -0600

    Update and rename README.md to readme.rs

--- README.md
+++
@@ -1,2 +0,0 @@
-# Channel.DIY-projects
-// Inspire. let home = 'https://www.youtube.com/@diyprojects7868'; video = [('This homemade craft is gaining popularity every day !','https://youtu.be/evcFMKByvEQ', ('You've been throwing away hundreds of dollars without even suspecting anything.','https://youtu.be/5cr5vJNAMho')]
---
+++ readme.rs
@@ -0,0 +1,7 @@
+/*# Channel.DIY-projects
+community: Inspire.
+*/
+
+let home = 'https://www.youtube.com/@diyprojects7868';
+
+video = [('This homemade craft is gaining popularity every day !','https://youtu.be/evcFMKByvEQ', ('You've been throwing away hundreds of dollars without even suspecting anything.','https://youtu.be/5cr5vJNAMho')]

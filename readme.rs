0085cb2a656db38f516ff23b82713f8fc05d4963
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-27 00:24:42 -0600

    Update readme.rs

--- readme.rs
+++ readme.rs
@@ -4,4 +4,8 @@ community: Inspire.
 
 let home = 'https://www.youtube.com/@diyprojects7868';
 
-video = [('This homemade craft is gaining popularity every day !','https://youtu.be/evcFMKByvEQ', ('You've been throwing away hundreds of dollars without even suspecting anything.','https://youtu.be/5cr5vJNAMho')]
+video = [
+  ('This homemade craft is gaining popularity every day !','https://youtu.be/evcFMKByvEQ'),
+  ('You've been throwing away hundreds of dollars without even suspecting anything.','https://youtu.be/5cr5vJNAMho'),
+  ('Brilliant ideas that you can make yourself in the garage','https://youtu.be/m3S6HT7C_A8'),
+]
